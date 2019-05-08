0d7d2364124cff059f9d86fd2b9aeaa4883a4e95
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2019-05-08 16:18:10 +0300

    Remind about lunch the last evening instead of morning, pair the lunchees in the morning

--- messages.py
+++ messages.py
@@ -11,7 +11,7 @@ Lunch Lottery is conducted every day at {}.""".format(LOTTERY_AT)
 TALLY = """There are {} people using this bot!"""
 
 REMINDER = """Lunch Lottery is conducted at {}.
-If you don't want to take part today, use /skip""".format(LOTTERY_AT)
+If you don't want to take part to the next lunch pairing, use /skip""".format(LOTTERY_AT)
 
 LUNCH = """Today you'll be having lunch with @{}!
 Send them a message!"""
